c43c3c25340ab3fdad37e3653580b305b3e84c46
Gelvanne <gelvanne@msn.com>
2020-01-06 22:26:06 -0300

    Modelos de HTML+CSS+PHP

---
+++ TelaLogin/cadastrar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Modelo|Login</title>
+<link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+<div id="corpo-form-cad">
+<h1>Cadastrar</h1>
+	<form action="processa.php" method="POST">
+		<input type="text" placeholder="Nome Completo"> 
+		<input type="text" placeholder="Telefone">
+		<input type="email" placeholder="E-mail">
+		<input type="password" placeholder="Senha">
+		<input type="password" placeholder="Confirma Senha"> 
+		<input type="submit" value="Cadastrar">
+		<a href="index.php"><strong>Retornar a home.</strong></a>
+		</form>
+</div>
+</body>
+</html>
---
+++ TelaLogin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Modelo|Login</title>
+<link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+<div id="corpo-form">
+<h1>Entrar</h1>
+	<form action="processa.php" method="POST">
+		<input type="email" placeholder="Usuario"> 
+		<input type="password" placeholder="Senha"> 
+		<input type="submit" value="Acessar">
+		<a href="cadastrar.php">Ainda não é inscrito? <strong>Cadastre-se!</strong></a>
+	</form>
+</div>
+</body>
+</html>
